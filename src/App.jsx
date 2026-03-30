0e6361de6b498e03fd9cd0fa32d1d8cd86b46534
antick-arch <e241003@grad.iiuc.ac.bd>
2026-03-30 21:49:29 +0600

    initialized with all basic files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Navbar from './components/navbar/Navbar'
+
+function App() {
+
+  return (
+    <>
+    <header>
+      <nav>
+        <Navbar></Navbar>
+      </nav>
+    </header>
+    </>
+  )
+}
+
+export default App
